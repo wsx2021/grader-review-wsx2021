4c22eb739800d630950e6490d5326705c3e9e533
wsx2021 <80902225+wsx2021@users.noreply.github.com>
2023-03-01 11:46:27 -0800

    update

--- TestListExamples.java
+++ TestListExamples.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
+
 
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
@@ -18,4 +20,17 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test 
+	public void testList() {
+    List<String> input = new ArrayList<>();
+    input.add("moon");
+    input.add("b");
+    input.add("c");
+    StringChecker sc = new IsMoon();
+    List<String> output = new ArrayList<>();
+    output.add("moon");
+    assertArrayEquals(output.toArray(), ListExamples.filter(input, sc).toArray());
+	}
+
 }
